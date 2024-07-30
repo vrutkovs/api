62c9c68fcc91551f655d2708b2baf27a0f23a190
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-07-30 13:07:16 +0200

    features: add ShortCertRotation

--- features/features.go
+++ features/features.go
@@ -718,4 +718,12 @@ var (
 					enhancementPR("https://github.com/openshift/enhancements/pull/1492").
 					enableIn(configv1.DevPreviewNoUpgrade).
 					mustRegister()
+
+	FeatureShortCertRotation = newFeatureGate("ShortCertRotation").
+					reportProblemsToJiraComponent("kube-apiserver").
+					contactPerson("vrutkovs").
+					productScope(ocpSpecific).
+					enableIn(configv1.DevPreviewNoUpgrade).
+					enhancementPR("https://github.com/openshift/enhancements/pull/1670").
+					mustRegister()
 )
